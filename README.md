df930cdce1d5c621dfdefbb3575e227f9e39b5fe
kad-bloemgx <gerard.bloemendaal@kadaster.nl>
2024-11-27 15:55:42 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Uitlezen APS ECU en converteren naar een JSON document
+
+sudo apt install python3-full
+https://www.jetbrains.com/help/fleet/running-code-using-a-virtual-environment.html
+
+
+How to run:
+python solarhtml2json.py  --url http://192.168.1.70/cgi-bin/parameters
+python solarhtml2json1.py  --url http://192.168.1.70/cgi-bin/home
---
+++ solarhtml2json1.py
@@ -0,0 +1,127 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+import os
+import argparse
+import re
+
+def clean_value(value, replace_dict, is_temperature=False):
+    for key, replacement in replace_dict.items():
+        value = value.replace(key, replacement)
+    cleaned_value = value.strip()
+    if is_temperature:
+        cleaned_value = re.sub(r'[^\d.]+', '', cleaned_value)  # Remove everything except digits and dot
+    return '0' if cleaned_value in ['', '-', '...'] else cleaned_value
+
+def get_html_content(file_path, url):
+    if file_path and os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            html_content = f.read()
+    else:
+        r = requests.get(url)
+        if r.status_code != 200:
+            print('Error:', r.status_code)
+            return None
+        html_content = r.text
+    return html_content
+
+def parse_table(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find('table')
+
+    if table is None:
+        print('No table found')
+        return None
+
+    rows = table.find_all('tr')
+    power_data = {}
+
+    latest_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    for row in rows[0:]:
+        columns = row.find_all('td')
+        if len(columns) == 2:
+            omschrijving = columns[0].text.strip()
+            power_data[omschrijving] = [clean_value(columns[1].text, {'kWh': '', 'W':''})]
+
+    return power_data
+
+def save_power_data(power_data):
+    with open('www/power_data1.json', 'w') as outfile:
+        json.dump(power_data, outfile, indent=4)
+
+def generate_yaml_from_json():
+    with open('www/power_data1.json', 'r') as f:
+        power_data = json.load(f)
+
+    inverter_ids = power_data.keys()
+
+    config = 'rest:\n'
+    config += '  - resource: http://homeassistant.local:8123/local/power_data1.json\n'
+    config += '    sensor:\n'
+
+    panel_names = set()
+
+    for id in inverter_ids:
+        panel_name = id.split('-')[0]
+        suffix = id.split('-')[1]
+        panel_names.add(f'solar_panel_{panel_name.lower()}_{suffix.lower()}')
+        unique_id_base = f"solar_panel_{panel_name.lower()}_{suffix.lower()}_"
+        config += (
+            f'      - name: "Solar Panel {panel_name} {suffix} power"\n'
+            f'        unique_id: "{unique_id_base}power"\n'
+            f'        value_template: \'{{{{ value_json["{id}"][0] }}}}\'\n'
+            f'        unit_of_measurement: "W"\n'
+            f'      - name: "Solar Panel {panel_name} {suffix} grid frequency"\n'
+            f'        unique_id: "{unique_id_base}grid_frequency"\n'
+            f'        value_template: \'{{{{ value_json["{id}"][1] }}}}\'\n'
+            f'        unit_of_measurement: "Hz"\n'
+            f'      - name: "Solar Panel {panel_name} {suffix} grid voltage"\n'
+            f'        unique_id: "{unique_id_base}grid_voltage"\n'
+            f'        value_template: \'{{{{ value_json["{id}"][2] }}}}\'\n'
+            f'        unit_of_measurement: "V"\n'
+            f'      - name: "Solar Panel {panel_name} {suffix} temperature"\n'
+            f'        unique_id: "{unique_id_base}temperature"\n'
+            f'        value_template: \'{{{{ value_json["{id}"][3] }}}}\'\n'
+            f'        unit_of_measurement: "..C"\n'
+        )
+    config += '\nsensor:\n'
+    config += '  - platform: template\n'
+    config += '    sensors:\n'
+    config += '      solar_panels_total_power:\n'
+    config += '        unique_id: "total_solar_power"\n'
+    config += '        friendly_name: "Total Solar Power"\n'
+    config += '        unit_of_measurement: "W"\n'
+
+    panels_sum = ' +\n             '.join([f"states('sensor.{name}_power')|float(default=0)" for name in panel_names])
+    config += (
+        '        value_template: >\n'
+        '          {{ (\n'
+        f'             {panels_sum}\n'
+        '          ) }}\n'
+    )
+
+    with open('config_part.yaml', 'w') as f:
+        f.write(config)
+
+def main():
+    parser = argparse.ArgumentParser(description='Process and collect solar data.')
+    parser.add_argument('--file', type=str, help='Path to local HTML file')
+    parser.add_argument('--url', type=str, help='URL to fetch the HTML content')
+    parser.add_argument('--ecu_v4', action='store_true', help='Enable ECU v4 parsing mode')
+    parser.add_argument('--generate_config', action='store_true', help='Generate config_part.yaml from power_data.json')
+
+    args = parser.parse_args()
+
+    if args.generate_config:
+        generate_yaml_from_json()
+    else:
+        html_content = get_html_content(args.file, args.url)
+        if html_content:
+            power_data = parse_table(html_content)
+            if power_data:
+                save_power_data(power_data)
+
+if __name__ == "__main__":
+    main()
